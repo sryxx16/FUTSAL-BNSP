5adfd30226ee4c5886357b66fd5ef7df3b2a871b
Surya <namaandaa72@gmail.com>
2026-07-20 16:39:39 +0700

    Fix: Add .next to gitignore and remove from cache

--- .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_1q6ehb4._.js","static/chunks/_1ao6z9d._.js"],
-    source: "dynamic"
-});
--- .next/server/chunks/ssr/_next-internal_server_app_login_page_actions_0p125k5.js
+++
@@ -1,3 +0,0 @@
-module.exports=[52055,a=>{"use strict";var b=a.i(61469);a.s([],83333),a.i(83333),a.s(["60d2ecea429cdb72155bdee8b7a0bd7ede6ced06dd",()=>b.loginUser,"78ebf4abbb6a8434342bdb907989b650e14836afa3",()=>b.registerUser],52055)}];
-
-//# sourceMappingURL=_next-internal_server_app_login_page_actions_0p125k5.js.map
